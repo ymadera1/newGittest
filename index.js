2b3e583dcd391ceca9e36a5d1db7b7d4e45feed8
Yvonne Madera <yvonnemadera@Macintosh.local>
2016-02-12 13:57:12 -0500

    adding new files

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ angular.module('MyApp', [])
 
 .controller('MainController', function() {
 	var self = this;
+
+	self.onButtonClick= function() {
+		alert('you clicked the button');
+	}
+
+	<input 
+
+	self.username = 'tommy';
+
 	self.greeting = 'Hello, yous';
+
+	self.simpleArray = ['Item1' , 'Item2' , 'Item3', 'Item4'];
+
+	self.interArray = [{
+		name: 'george'
+	}, {
+		name: 'Harry'
+	}, {
+		name: 'Sally'
+	}];
+
+	self.states = {
+		CA: 'California',
+		NY: 'New York',
+		FL: 'Floriday'
+	};
 
 });
